8c8a5fcf905d4e227b538117233ff1f7bb906b67
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-16 00:02:26 +0530

    update

---
+++ InputOutput/first.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+   cout<<"Enter the 1st number";
+   cin>>num1;
+   cout<<"Enter the 2st number";
+   cin>>num2;
+
+   cout<<"The sum is "<<num1+num2;
+}
