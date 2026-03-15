93b551ed43a03f3487bb566d2f2046e40fe8b271
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-15 23:33:01 +0530

    update

---
+++ DataType/first.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=4,b=5;
+    // int b=5;
+    float pi=3.14;
+    char c='u';
+    cout<<"The value is "<<a+b;
+    cout<<"\nThe value is "<<pi;
+    cout<<"\nThe value is "<<c;
+
+
+    return 0;
+
+}
