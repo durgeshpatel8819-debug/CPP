d14305f98c21213daf7536704b63c751729a8187
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-15 23:17:05 +0530

    update

---
+++ Variable/first.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int sum=5;//global variable
+int main(){
+    int sum = 6;//local variable
+    cout<<sum ;
+    cout<<"Hello world";
+    cout<<sum;
+    return 0;
+    
+}
