2e69c0c70da2ee8700c9ce6be21dbaec61be0bef
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-17 00:30:24 +0530

    update

---
+++ HeaderFile/first.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+//There are two types of header file
+// 1.Sysyem header files: it comes with compiler
+// 2. User defined header file: it written by the programmer
+// #include "this.h" --> This will produce an error if this.h is no present in current directory
+using namespace std;
+int main(){
+    cout<<"Hello world";
+    return 0;
+}
