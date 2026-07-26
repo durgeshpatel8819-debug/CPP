3bf04d80b9afea7fd8ac7faab307907f51ddbeaa
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-07-26 15:35:02 +0530

    Adittion of number

---
+++ DataType/second.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x,y;
+cout<<"Enter the first number:";
+cin>>x;
+cout<<"Enter the second number :";
+cin>>y;
+cout<<"The sum is :"<<(x+y);
+
+
+    return 0;
+} 
