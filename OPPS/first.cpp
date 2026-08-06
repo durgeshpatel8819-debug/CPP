fc7d7ebb29d208cf64901476cabd3429506e7869
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-08-06 12:34:16 +0530

    update

---
+++ OPPS/first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class student{
+    public :
+    string name;
+    int age;
+    void display(){
+        cout<<"Name :"<<name<<",Age :"<<age<<endl;
+    }
+};
